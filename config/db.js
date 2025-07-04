e01e219aaa3341d7a770e322fd0ad10a70d9e069
mayankchugh17 <mayankchugh222@gmail.com>
2025-07-04 21:48:58 +0530

    First Commit

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    
+    try{
+        await mongoose.connect(process.env.DB_URL)
+        console.log("MongoDB connected successfully");  
+    }
+    catch(error)
+    {
+        console.error("Error connecting to MongoDB:", error.message);
+        process.exit(1); // Exit the process with failure
+    }
+}
+
+export default connectDB;
